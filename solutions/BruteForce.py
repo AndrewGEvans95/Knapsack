cc37d0e88e1b95a29c5c792c71fbb72ee034b95f
AndrewGEvans95 <Andrewgevans95@gmail.com>
2017-01-12 23:11:37 -0500

    Updated BruteForce solution and added Dynamic Programming Solution
    Added new comments and output options to BruteForce solution.  Added a
    dynamic programming based solution to the 0-1 Knapsack problem.

--- solutions/BruteForce.py
+++ solutions/BruteForce.py
@@ -10,7 +10,7 @@
 #(weight, value)
 items = [(15, 2), (13, 5), (13, 9), (12, 5), (12, 9), (11, 7)]
 #Weight capacity
-W = 20
+W = 100
 
 def solve(items, weight):
 	#First we need every possible combination of our set
